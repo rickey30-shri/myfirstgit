335dff5930ce8c802bfe821c9f20bd98c561e719
aakash <rickey30.shri@gmail.com>
2024-09-03 15:23:12 +0530

    test

---
+++ srever/server.py
@@ -0,0 +1,13 @@
+import socket
+s = socket.socket()
+print('Socket Created')
+s.bind(('localhost' ,9999))
+s.listen(3)
+print("waiting for connections")
+
+while True:
+    c, addr=s.accept()
+    name= c.recv(1024).decode()
+    print("connected with",addr,name)
+    c.send(bytes('welcome to aakash','utf-8'))
+    c.close()
